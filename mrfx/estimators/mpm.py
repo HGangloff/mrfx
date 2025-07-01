5e8329d2dea8ce337f700b6e2942c5c8d6e21425
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2025-07-01 19:08:02 +0200

    Add Gibbs a posteriori notebook and code improvements

---
+++ mrfx/estimators/mpm.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+from jaxtyping import Int, Array, Key
+
+from mrfx.utils.utils import get_most_frequent_values
+
+if TYPE_CHECKING:
+    from mrfx.samplers import AbstractSampler
+    from mrfx.models import AbstractMarkovRandomFieldModel
+    
+
+def mpm_estimator(
+    sampler: AbstractSampler,
+    model: AbstractMarkovRandomFieldModel,
+    K: int,
+    n: int,
+    key: Key
+) -> Int[Array]:
+    """
+    Get the maximum posterior mode estimate of a MRF
+    """
+    # Silence the sampler
+    sampler = eqx.tree_at(
+        lambda pt: pt.verbose,
+        sampler,
+        False
+    )
+    def one_simulation(key):
+        return sampler.run(model, key)[1][-1]
+        
+    keys = jax.random.split(key, n)
+    samples = jax.vmap(one_simulation)(keys)
+    return get_most_frequent_values(samples, K)
--- mrfx/models/_abstract.py
+++ mrfx/models/_abstract.py
@@ -4,6 +4,8 @@ Abstract MRF model
 
 import abc
 from jaxtyping import Float, Int, Array, Key
+import jax
+import jax.numpy as jnp
 import equinox as eqx
 
 
@@ -16,13 +18,15 @@ class AbstractMarkovRandomFieldModel(eqx.Module):
     )  # default is 8 nearest neighbors
 
     @abc.abstractmethod
-    def potential(self, x: Array, neigh_values: Array) -> Float:
+    def potential(self, x: Array, neigh_values: Array, u: Array, v: Array) -> Float:
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def potential_values(self, neigh_values: Array) -> Array:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def sample(self, potential_values: Array, key: Key) -> Array:
-        raise NotImplementedError
+    def potential_values(self, neigh_values: Array, u: Array, v: Array) -> Array:
+        vmap_potential = jax.vmap(self.potential, (0, None, None, None))
+        potential_values = jnp.exp(vmap_potential(jnp.arange(self.K), neigh_values, u, v))
+        return potential_values / potential_values.sum()
+    
+    def sample(self, potential_values: Array, key: Key) -> Int:
+        r = jax.random.uniform(key)
+        potential_cum = jnp.cumsum(potential_values)
+        return jnp.count_nonzero(potential_cum < r)
